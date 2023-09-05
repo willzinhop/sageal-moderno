196b86ce524c340840c909778e888d2e4a4f1d48
irys myrella <irysmyrella30@gmail.com>
2023-09-05 20:05:33 -0300

    dados pessoais concluido

--- database/create/usuarios.php
+++ database/create/usuarios.php
@@ -1,20 +1,23 @@
 <?php
-require_once("../connect.php");
 
-$sql = "CREATE TABLE aluno (
+$sql = "CREATE TABLE alunos (
     id_usuario INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    cpf VARCHAR (11),
-    senha VARCHAR (30),
-    email VARCHAR (50),
-    token VARCHAR (244)
+    cpf VARCHAR(11),
+    senha VARCHAR(30),
+    email VARCHAR(50),
+    token VARCHAR(244),
+    id_dados INT(10) UNSIGNED NOT NULL,
+    FOREIGN KEY (id_dados) REFERENCES alunos (id_usuario)    
+)"; 
 
-    
-)";
 
-if(mysqli_query($sageal, $sql)) {
+require_once("../connect.php");
+$resultado = mysqli_query($sageal, $sql);
+
+if ($resultado) {
     echo("Funcionou");
 }
 else{
-    echo("Não está rodando");
+    echo 'Não está rodando: ' . mysqli_error;
 }
 ?>
